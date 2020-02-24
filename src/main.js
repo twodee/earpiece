2f8609cc3348865f48d8aa1aa62ef09c110ba5f8
Chris Johnson <crjjrc@gmail.com>
2020-02-24 13:58:09 -0600

    Add piece-wise effect generator.

--- src/main.js
+++ src/main.js
@@ -54,7 +54,7 @@ function generateWav(samples) {
   putInt(h);
 
   for (let i = 0; i < samples.length; ++i) {
-    putSample(samples[i]);
+    putSample(parseInt(samples[i] * 32767));
   }
 
   let binary = '';
@@ -77,16 +77,34 @@ function getRandom() {
   return samples;
 }
 
-function sine() {
+function sine(p) {
+  return Math.sin(2 * Math.PI * p);
+}
+
+function square(duty) {
+  return p => {
+    return p >= duty ? 0 : 1;
+  };
+}
+
+function sawtooth(p) {
+  return p;
+}
+
+function triangle(p) {
+  return 2 * (0.5 - Math.abs(0.5 - p));
+}
+
+function sample(f) {
   const frequencies = [440, 880, 1000, 1500];
-  const clipLength = 100000;
+  const clipLength = 10000;
   let samples = new Array(frequencies.length * clipLength);
   let p = 0;
 
   for (let [fi, frequency] of frequencies.entries()) {
     let cyclesPerSample = frequency / 22050.0;
     for (let si = fi * clipLength; si < (fi + 1) * clipLength; ++si) {
-      samples[si] = Math.sin(2 * Math.PI * p) * 32767;
+      samples[si] = f(p) * 32767;
       p += cyclesPerSample;
       if (p >= 1) {
         p -= 1;
@@ -97,4 +115,74 @@ function sine() {
   return samples;
 }
 
-generateWav(sine());
+function generateSamples(effect) {
+  const nsamples = Math.round(effect.duration * effect.rate);
+  let samples = new Array(nsamples);
+  let p = 0;
+
+  let frequencyIndex = 0;
+  let amplitudeIndex = 0;
+
+  for (let sampleIndex = 0; sampleIndex < nsamples; sampleIndex += 1) {
+    const proportion = sampleIndex / (nsamples - 1);
+
+    // Advance to next pieces as needed.
+    if (proportion > effect.frequencies[frequencyIndex + 1][0]) {
+      frequencyIndex += 1;
+    }
+    if (proportion > effect.amplitudes[amplitudeIndex + 1][0]) {
+      amplitudeIndex += 1;
+    }
+
+    const frequencyA = effect.frequencies[frequencyIndex];
+    const frequencyB = effect.frequencies[frequencyIndex + 1];
+    const amplitudeA = effect.amplitudes[amplitudeIndex];
+    const amplitudeB = effect.amplitudes[amplitudeIndex + 1];
+
+    // Interpolate frequency.
+    const frequencyT = (proportion - frequencyA[0]) / (frequencyB[0] - frequencyA[0]);
+    const frequency = (1 - frequencyT) * frequencyA[1] + frequencyT * frequencyB[1];
+
+    // Interpolate amplitude.
+    const amplitudeT = (proportion - amplitudeA[0]) / (amplitudeB[0] - amplitudeA[0]);
+    const amplitude = (1 - amplitudeT) * amplitudeA[1] + amplitudeT * amplitudeB[1];
+
+    const cyclesPerSample = frequency / effect.rate;
+
+    samples[sampleIndex] = effect.generator(p) * amplitude; p += cyclesPerSample; if (p >= 1) {
+      p -= 1;
+    }
+  }
+
+  return samples;
+}
+
+const effect = {
+  rate: 11025,
+  generator: sawtooth,
+  duration: 1,
+  frequencies: [
+    [0, 880],
+    [0.5, 880],
+    [0.75, 3000],
+    [1, 0],
+  ],
+  amplitudes: [
+    [0, 1],
+    [0.5, 1],
+    [1, 0],
+  ],
+};
+
+generateWav(generateSamples(effect));
+
+// generateWav(sample(sine));
+// generateWav(sample(sawtooth));
+// generateWav(sample(triangle));
+// generateWav(sample(square(0.1)));
+// generateWav(sample(square(0.2)));
+// generateWav(sample(square(0.4)));
+// generateWav(sample(square(0.5)));
+// generateWav(sample(square(0.6)));
+// generateWav(sample(square(0.8)));
+// generateWav(sample(square(0.95)));
